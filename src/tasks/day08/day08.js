3d83752f946f45df99af241cd466b1f267eb613b
Alexandr Loginov <atatakobry@gmail.com>
2019-12-11 22:55:24 +0200

    day 08 pt. 2

--- src/tasks/day08/day08.js
+++ src/tasks/day08/day08.js
@@ -1,4 +1,5 @@
-import { chunk, map, split, trim, join, filter, flattenDeep, minBy } from 'lodash';
+import React from 'react';
+import { chunk, map, split, trim, filter, flattenDeep, minBy, fill } from 'lodash';
 
 import input from './input.txt';
 
@@ -10,11 +11,30 @@ export const getLayers = (input, width = 25, height = 6) => chunk(
     height
 );
 
-export const getImage = layers => join(flattenDeep(layers), '');
-
 export const getNumberOf = (layer, n) => filter(flattenDeep(layer), d => d === n).length;
 
 export const getNecessaryLayer = layers => minBy(layers, layer => getNumberOf(layer, 0));
+
+export const mergeLayers = layers => {
+    const width = layers[0][0].length;
+    const height = layers[0].length;
+    const depth = layers.length;
+
+    const mergedLayer = map(new Array(height), () => fill(new Array(width), 2));
+
+    for (let n = 0; n < width; n++) {
+        for (let m = 0; m < height; m++) {
+            for (let l = 0; l < depth; l++) {
+                if (layers[l][m][n] === 0 || layers[l][m][n] === 1) {
+                    mergedLayer[m][n] = layers[l][m][n];
+                    break;
+                }
+            }
+        }
+    }
+
+    return mergedLayer;
+};
 
 export default {
     day: 8,
@@ -26,5 +46,32 @@ export default {
 
         return getNumberOf(layer, 1) * getNumberOf(layer, 2);
     },
-    answer2: () => '-'
+    answer2: () => {
+        const layers = getLayers(input);
+        const mergedLayer = mergeLayers(layers);
+
+        return (
+            <span style={{
+                display: 'inline-flex',
+                verticalAlign: 'middle',
+                flexWrap: 'wrap',
+                width: '50px',
+                height: '12px'
+            }}>
+                {
+                    mergedLayer.map((row, m) =>
+                        row.map((element, n) =>
+                            <span key={`${m},${n}`} style={{
+                                display: 'block',
+                                width: '2px',
+                                height: '2px',
+                                margin: 0,
+                                padding: 0,
+                                backgroundColor: element ? 'white' : 'black'
+                            }}/>)
+                    )
+                }
+            </span>
+        );
+    }
 }
--- src/tasks/day08/day08.test.js
+++ src/tasks/day08/day08.test.js
@@ -1,21 +1,19 @@
-import { getLayers, getImage, getNumberOf, getNecessaryLayer } from './day08';
+import { getLayers, getNumberOf, getNecessaryLayer, mergeLayers } from './day08';
 
 test('get layers', () => {
     const input = `
     123456789012
     `;
     expect(getLayers(input, 3, 2)).toStrictEqual([
-        [[1,2,3], [4,5,6]],
-        [[7,8,9], [0,1,2]]
+        [
+            [1,2,3],
+            [4,5,6]
+        ],
+        [
+            [7,8,9],
+            [0,1,2]
+        ]
     ]);
-});
-
-test('get image', () => {
-    const layers = [
-        [[1,2,3], [4,5,6]],
-        [[7,8,9], [0,1,2]]
-    ];
-    expect(getImage(layers)).toBe('123456789012');
 });
 
 test('get number of', () => {
@@ -26,8 +24,66 @@ test('get number of', () => {
 
 test('get necessary layer', () => {
     const layers = [
-        [[1,2,3], [4,5,6]],
-        [[7,8,9], [0,1,2]]
+        [
+            [1,2,3],
+            [4,5,6]
+        ],
+        [
+            [7,8,9],
+            [0,1,2]
+        ]
     ];
     expect(getNecessaryLayer(layers)).toStrictEqual([[1,2,3], [4,5,6]]);
+});
+
+test('merge layers', () => {
+    let input;
+    let layers;
+
+    input = `
+    0222112222120000
+    `;
+    layers = getLayers(input, 2, 2);
+    expect(mergeLayers(layers)).toStrictEqual([
+        [0,1],
+        [1,0]
+    ]);
+
+    layers = [
+        [
+            [2,2,1],
+            [1,1,0]
+        ],
+        [
+            [0,2,0],
+            [2,2,2]
+        ],
+        [
+            [2,1,2],
+            [0,0,0]
+        ]
+    ];
+    expect(mergeLayers(layers)).toStrictEqual([
+        [0,1,1],
+        [1,1,0]
+    ]);
+
+    layers = [
+        [
+            [2,2,2],
+            [2,2,2]
+        ],
+        [
+            [0,2,0],
+            [2,2,2]
+        ],
+        [
+            [2,1,2],
+            [0,0,0]
+        ]
+    ];
+    expect(mergeLayers(layers)).toStrictEqual([
+        [0,1,0],
+        [0,0,0]
+    ]);
 });
